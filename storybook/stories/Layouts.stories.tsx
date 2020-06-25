1c70ac0675b93adfdce359374c1738fe556e46d3
Odee <odjhey@gmail.com>
2020-06-25 08:39:41 +0800

    refactor(storybook): use storiesOf instead of a direct export

--- storybook/stories/Layouts.stories.tsx
+++ storybook/stories/Layouts.stories.tsx
@@ -1,17 +1,17 @@
 import React from "react"
+
+import { storiesOf } from "@storybook/react"
+
 import { EPODWelcome, SideBySide } from "@epod/ui"
 
-export default {
-  title: "Layouts",
-}
-
-// By passing optional props to this story, you can control the props of the component when
-// you consume the story in a test.
-export const Default = (props?: Partial<any>) => (
-  <SideBySide
-    Left={<EPODWelcome message={"mina"} />}
-    leftStyle={{ border: "solid 1px black" }}
-    Right={<div>Lorem ipsum</div>}
-    rightStyle={{ backgroundColor: "red" }}
-  />
+storiesOf("Layouts", module).add(
+  "Simple side by side",
+  (props?: Partial<any>) => (
+    <SideBySide
+      Left={<EPODWelcome message={"mina"} />}
+      leftStyle={{ border: "solid 1px black" }}
+      Right={<div>Lorem ipsum</div>}
+      rightStyle={{ backgroundColor: "red" }}
+    />
+  ),
 )
